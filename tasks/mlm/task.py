96303cb8fe22b2502d3fc4f07cbe2d638140aef9
justheuristic <justheuristic@gmail.com>
2022-01-03 07:20:03 +0300

    TODO try without sharing memory

--- tasks/mlm/task.py
+++ tasks/mlm/task.py
@@ -164,6 +164,7 @@ class MLMTrainingTask:
         If ramp-up is enabled, start with smaller sequences of initial_sequence_length tokens, then increase linearly
         to the max_sequence_length over the period of first
         """
+        return
         current_epoch = self.collaborative_optimizer.tracker.global_epoch
         if self.trainer_args.sequence_length_warmup_steps == 0 or current_epoch > self.trainer_args.sequence_length_warmup_steps:
             current_sequence_length = self.trainer_args.max_sequence_length
